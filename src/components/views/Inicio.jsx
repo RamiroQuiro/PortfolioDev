463c21a35b96cc313a4733dd3427bc2049834448
ramirochangomoreno <rama.exe.13@gmail.com>
2022-06-29 10:27:40 -0300

    actualizando google analityc

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -7,10 +7,10 @@ export default function Inicio({}) {
       <div className=''>
     <img
       className={
-        " opacity-100 duration-300 ease-in-out transition-all -z-10  flex justify-center items-center mx-auto scale-150 md:scale-75 h-full -rotate-12"
+        " opacity-100 duration-300 ease-in-out transition-all -z-10  flex justify-center items-center mx-auto scale-150 md:scale-100 h-full -rotate-12"
       }
-      width="100%"
-      height="100%"
+      width="892px"
+      height="1336px"
       src={image2}
       alt="RamaCode"
     /></div>
